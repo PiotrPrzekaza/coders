993c026370e52ac4327c214428c4a836e1b4c1df
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-06-24 18:26:35 +0200

    added some variables and some question to secure in backend

--- app.js
+++ app.js
@@ -5,3 +5,36 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server is listening at http://localhost:3000');
 });
+
+let goodAnswers = 0;
+let callToFrendUsed = false;
+let questionToAudianceUsed = false;
+let fiftyFiftyUsed = false;
+
+const questions = [
+    {
+        question: 'Gdzie w pamięci przechowywane są typy proste a gdzie typy referencyjne',
+        answers: ["Oba typy są przechowywane na stercie", "Typy proste na stercie, a referaencyjne na stosie", "Oba typy są przechowywane na stosie", "Typy proste na stosie, a referencyjne na stercie"],
+        correctAnswer: 3,
+    },
+    {
+        question: 'W jakim celu stosuje się dziedziczenie?',
+        answers: ["Kiedy potrzebujemy aby z klasy dziedziczącej można było utworzyć obiekt", 'Aby przekazać klasie dziedziczącej "zachowania" klasy dziedziczonej', "Aby utrudnic innym programistom odczytanie naszego kodu", "Kiedy potrzebujemy aby z klasy dziedziczonej mozna było utworzyć obiekt"],
+        correctAnswer: 1,
+    },
+    {
+        question: 'Po ilu klasach abstrakcyjnych można dzidziczyć?',
+        answers: ["Po jednej", "Nie istnieją klasy abstrakcyjne w C#", "Nie ma ograniczeń", "Po dwóch"],
+        correctAnswer: 0,
+    },
+    {
+        question: 'Jaki jest domyślny modificator dostępu w interfejsie?',
+        answers: ["sealed", "private", "normal", "public"],
+        correctAnswer: 3,
+    },
+    {
+        question: 'Jaka jest różnica między klasą a obiektem?',
+        answers: ["Obiekt dziedziczy po klasie", "Nie ma różnic, obiekt to to samo co klasa", "Klasa jest definicją na podstawie której możemy utworzyć obiekt", "Klasa jest tworzona w czasie wykonywania programu, a obiekt podczas kompilacji"],
+        correctAnswer: 2,
+    }
+]
